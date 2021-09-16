bd9fb03683d8fc90e832213772dfeff67fd23e10
tilleyarthur <86635878+tilleyarthur@users.noreply.github.com>
2021-09-16 19:22:01 -0400

    arrow function

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+  }
+  console.log('================');
+  // is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem)); {
+    console.log(profileItem)
+  };
+};
+
+
+  
